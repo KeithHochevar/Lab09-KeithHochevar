e4bea41d952d287ca36b3ca5b90806067b40d144
KeithHochevar <36213289+KeithHochevar@users.noreply.github.com>
2018-05-08 15:24:55 -0400

    Add files via upload

---
+++ Lab9/Lab9/MyMath.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab9
+{
+    class MyMath
+    {
+        double result;
+        double operand1;
+        double operand2;
+
+        public void Multiply(double operand1, double operand2)
+        {
+            result = (operand1 * operand2);
+        }
+
+        public void Divide(double operand1, double operand2)
+        {
+            result = (operand1 / operand2);
+        }
+
+        public void Subtract(double operand1, double operand2)
+        {
+            result = (operand1 - operand2);
+        }
+
+        public void Add(double operand1, double operand2)
+        {
+            result = (operand1 + operand2);
+        }
+
+        public double GetResult()
+        {
+            return result;
+        }
+    }
+}
---
+++ Lab9/Lab9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath MyMath = new MyMath();
+
+            MyMath.Multiply(1, 2);
+            Console.WriteLine(MyMath.GetResult());
+
+            MyMath.Divide(2, 3);
+            Console.WriteLine(MyMath.GetResult());
+
+            MyMath.Subtract(3, 4);
+            Console.WriteLine(MyMath.GetResult());
+
+            MyMath.Add(4, 5);
+            Console.WriteLine(MyMath.GetResult());
+        }
+    }
+}
